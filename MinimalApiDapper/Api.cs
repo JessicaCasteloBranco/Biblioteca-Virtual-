0a2a8d1c48c2e12ec2d92002b4e24b5fe07fbe31
Jéssica Castelo Branco <jessicacastelobranco1914@gmail.com>
2023-06-30 09:07:55 -0400

    Mapeamento dos endpoints

---
+++ MinimalApiDapper/Api.cs
@@ -0,0 +1,100 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MinimalApiDapper
+{
+    public static class Api
+    {
+        public static void ConfigureApi(this WebApplication app)
+        {
+            //aqui é onde vou fazer meu mapeamento (endpoint) na API
+            app.MapGet(pattern: "/Users", GetUsers);
+            app.MapGet(pattern: "/Useres/{id}", GetUser);
+            app.MapPost(pattern: "/Users", InsertUser);
+            app.MapPut(pattern: "/Users", UpdateUser);
+            app.MapDelete("/Users", DeleteUser);
+        }
+
+        private static async Task<IResult> GetUsers(IUserData data)
+        {
+
+            try
+            {
+                return Results.Ok(await data.GetUsers());
+            }
+            catch (Exception ex){
+
+                return Results.Problem(ex.Message);
+            
+            }   
+
+        }
+
+        private static async Task <IResult> GetUser(int id, IUserData data)
+        {
+
+            try
+            {
+                var results = await data.GetUser(id);
+                if (results == null) return Results.NotFound();   
+                return Results.Ok(results); 
+            }
+            catch (Exception ex)
+            {
+
+                return Results.Problem(ex.Message);
+
+            }
+        }
+
+       private static async Task<IResult> InsertUser(UserModel user, IUserData data)
+        {
+            try
+            {
+                await data.InsertUser(user);
+                return Results.Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return Results.Problem(ex.Message);
+
+            }
+        }
+
+        private static async Task<IResult> UpdateUser (UserModel user, IUserData data)
+        {
+
+            try
+            {
+                await data.UpdateUser(user);
+                return Results.Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return Results.Problem(ex.Message);
+
+            }
+
+        }
+
+        private static async Task<IResult> DeleteUser(int id, IUserData data)
+        {
+
+            try
+            {
+                await data.DeleteUser(id);
+                return Results.Ok();
+            }
+            catch (Exception ex)
+            {
+
+                return Results.Problem(ex.Message);
+
+            }
+
+
+        }
+
+    }
+}
--- MinimalApiDapper/Program.cs
+++ MinimalApiDapper/Program.cs
@@ -1,9 +1,15 @@
+
+using DataAccess.DbAccess;
+using MinimalApiDapper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ISqlDataAccess, SqlDataAccess>();
+builder.Services.AddSingleton<IUserData, UserData>();
 
 var app = builder.Build();
 
@@ -16,29 +22,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
+app.ConfigureApi();
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
